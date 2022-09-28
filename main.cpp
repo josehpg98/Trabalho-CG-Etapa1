73806dbaaf3f822d1c63f137cf9b258aa3873256
José Henrique Paludo Giombelli <42421113+josehpg98@users.noreply.github.com>
2022-09-28 12:55:19 -0300

    testes 2

--- main.cpp
+++ main.cpp
@@ -7,78 +7,71 @@ using namespace std;
 using namespace Eigen;
 int main(void)
 {
+	int op_menu, seg, op_transf;
+	string imagem;
 	///imagem de entrada
 	PGM* imgE = new PGM();
 	///imagem de saida
 	PGM* imgS = new PGM();
 	///cria uma imagem pgm
-	imgS->criar(imgE->getLargura(), imgE->getAltura() );
 	///leitura da imagem
-	imgE->ler("numeros.pgm");
-	///int menu;
-	////do
-	///{
-		///system("cls");
-		///cout<<"\n 0  - Sair "<<endl;
-		///cout<<" 1  - Translação "<<endl;
-		///cout<<" 2  - Rotação "<<endl;
-		///cout<<" 3  - Escala "<<endl;
-		///cout<<" 4  - Cisalhamento horizontal "<<endl;
-		///cout<<" 5  - Cisalhamento vertical "<<endl;
-		///cout<<" 6  - Reflexão em torno do eixo x "<<endl;
-		///cout<<" 7  - Reflexão em torno do eixo y "<<endl;
-		///cout<<" \n Escolha uma Opcao: "<<endl;
-		///cin>>menu;
-		///fflush(stdin);
-		///switch (menu)
-		///{
-		///case 0:
-			///cout << "PROGRAMA FINALIZADO!";
-			///getchar();
-			///break;
-		///case 1:
-			///system("cls");
-			Trans2D::getMTranslacao(0,150);
-			imgS->gravar("saida.pgm");
-			///getchar();
-			///break;
-		///case 2:
-			///system("cls");
-			Trans2D::rotacao(45.0f,imgE, imgS);
-			imgS->gravar("saida.pgm");
-			///getchar();
-			///break;
-		///case 3:
-			///system("cls");
-			Trans2D::getMEscala(0.5, 0.5);
-			imgS->gravar("saida.pgm");
-			///getchar();
-			///break;
-		///case 4:
-			///system("cls");
-			///getchar();
-			///break;
-		///case 5:
-			///system("cls");
-			///getchar();
-			///break;
-		///case 6:
-			///getchar();
-			///break;
-		///case 7:
-			///system("cls");
-			///getchar();
-			///break;
-		///default:
-			///cout<<"Opção invalida!";
-			///getchar();
-			///break;
-		///}
-		///fflush(stdin);
-	///
-	//} while (menu != 0);
+	cout<<"Informe a imagem: "<<endl;
+	cin>>imagem;
+	fflush(stdin);
+	///cout<<imagem;
+	imgE->ler(imagem);
+	imgS->criar(imgE->getLargura(), imgE->getAltura() );
+	do{
+		cout<<"\n Digite 1 para aplicar uma transformação, ou 0 para sair: ";
+		cin>>op_menu;
+		fflush(stdin);
+		if(op_menu == 1){
+			do{
+			cout<<"\n Segmento (1, 2, 3 ou 4):";
+			cin>>seg;
+			fflush(stdin);
+			cout<<"\n Transformações Geometricas Disponiveis: "<<endl;
+			cout<<"1 - Rotação "<<endl;
+			cout<<"2 - Translação"<< endl;
+			cout<<"3 - Escala"<< endl;
+			cout<<"4 - Reflexão  "<< endl;
+			cout<<"5 - Cisalhamento"<< endl;
+			cout<<"0 - Sair"<<endl;
+			cin>>op_transf;
+			fflush(stdin);
+			switch(op_transf)
+			{
+				case 1: 
+				cout<<"Rotação: "<<endl;
+				Trans2D::rotacao(45.0f,imgE, imgS);
+				imgS->gravar("saida.pgm");
+				getchar();
+				break;
+				case 2: 
+				cout<<"Translação: "<<endl;
+				getchar();
+				break;
+				case 3: 
+				cout<<"Escala: "<<endl;
+				getchar();
+				break;
+				case 4: 
+				cout<<"Reflexão: "<<endl;
+				getchar();
+				break;
+				case 5: 
+				cout<<"Cisalhamento: "<<endl;
+				getchar();
+				break;
+			}
+			}while(op_transf != 0);
+		}else{
+			cout<<"\n Programa Finalizado! ";
+			getchar();
+		}
+	}while(op_menu != 0);
 	///deleta as imagens de entrada e saida.
-	delete imgE;
-	delete imgS;
+	///delete imgE;
+	///0delete imgS;
 	return EXIT_SUCCESS;
 }
